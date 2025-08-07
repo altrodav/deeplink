ec0af7492202825a7d9018ae1e42ceab9de3d95f
Ashutosh Kumar <132209941+AshutoshStark@users.noreply.github.com>
2025-08-07 13:28:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // Route for redirection
 app.get("/",(req,res)=>{
     res.status(200).json(
-        "welcome to the Meet-X"
+        "welcome to the Meet-mux"
     );
 });
 
@@ -13,10 +13,10 @@ app.get('/getapp', (req, res) => {
     
     if (/android/i.test(userAgent)) {
         // Redirect to Play Store if Android device
-        res.redirect('https://play.google.com/store/apps/details?id=com.profcymasolutions.meetx');
+        res.redirect('https://www.meetmux.com/');
     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
         // Redirect to App Store if iOS device
-        res.redirect('https://meetx.co.in/');
+        res.redirect('https://apps.apple.com/in/app/meetmux/id6747908089');
     } else {
         // Redirect to your website for unsupported platforms
         res.redirect('https://altrodav.com/');
